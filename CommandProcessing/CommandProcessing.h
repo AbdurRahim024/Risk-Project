4b891ca8866133ac174903db3930038c6089528f
Emma <3lddemmanounke@gmail.com>
2022-11-14 00:38:00 -0500

    Changes by Marin

--- CommandProcessing/CommandProcessing.h
+++ CommandProcessing/CommandProcessing.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef RISK_PROJECT_COMMANDPROCESSING_H
 #define RISK_PROJECT_COMMANDPROCESSING_H
 #include <stdio.h>
@@ -10,61 +11,105 @@ class Command;
 class FileLineReader;
 class FileCommandProcessorAdapter;
 
-class CommandProcessor
-{
+
+class CommandProcessor : public ILoggable, public Subject {
 private:
     vector <Command*> commandsList;
+    static LogObserver* obs;
+
 protected:
-    string readCommand();
+    virtual string readCommand();
     Command* saveCommand(string* command, string* effect);
+
 public:
-    Command* getCommand();
+
+
+    //stream insertion operator
+    friend ostream &operator << (ostream &output, CommandProcessor &obj);
+
+    //assignment operator
+    CommandProcessor& operator=(const CommandProcessor &commandProcessor); //dom changed
+
+    //constructors
+    CommandProcessor();
+
+    //copy constructor
+    CommandProcessor(CommandProcessor &commandProcessor);
+
+    //getters & setters
     vector <Command*> getCommandsList();
-    bool validate(string command);
+    int getListCommand();
+    static void setObserver(Observer* o);
+
+    //destructors
     ~CommandProcessor();
 
+    //methods
+    Command* getCommand();
+    bool validate(string command);
+
+    void Notify() override;
+    string stringToLog() override;
 };
 
-class Command
-{
+class Command : public ILoggable, public Subject {
 private:
     string* command;
     string* effect;
     static LogObserver* obs;
 
 public:
-    Command* saveEffect(string *effect);
+    //insertion stream operators
+    friend ostream &operator << (ostream &output, Command &obj);
 
-    //Constructor for command object
+    //assignment operator
+    Command &operator=(const Command &t); //dom changed
+
+    //Constructor
     Command();
     Command(string *command, string *effect);
 
-    //Getters
+    //copy constructor
+    Command(Command &command);
+
+    //Getters & Setters
     string* getEffect();
     string* getCommandString();
     static void setObserver(Observer* o);
 
-    //destructor
+    //Destructor
     ~Command();
 
-
+    //Methods
+    Command* saveEffect(string *effect);
+    void Notify() override;
+    string stringToLog() override;
 };
 
-class FileCommandProcessorAdapter : public CommandProcessor
+class FileCommandProcessorAdapter: public CommandProcessor
 {
 private:
     FileLineReader* flr;
     static int* counter;
-public:
-    FileCommandProcessorAdapter();
-    string readCommand();
-    Command* passCommand();
 
-    //destructor
+public:
+    //insertion stream operators
+    friend ostream &operator << (ostream &output, FileCommandProcessorAdapter &obj);
+
+    //assignment operators
+    FileCommandProcessorAdapter &operator=(const FileCommandProcessorAdapter &t);
+
+    //constructors
+    FileCommandProcessorAdapter();
+    //copy constructor
+    FileCommandProcessorAdapter(FileCommandProcessorAdapter &f);
+
+    //Destructor
     ~FileCommandProcessorAdapter();
 
-
-
+    //Methods
+    string readCommand();
+    Command* passCommand();
 
 };
 
@@ -72,16 +117,28 @@ class FileLineReader
 {
 private:
     vector <string*> rawCommands;
+
 public:
-    void readLineFromFile();
+    //Insertion stream operator
+    friend ostream &operator << (ostream &output, FileLineReader &obj);
+
+    //Assignment operator
+    FileLineReader &operator=(const FileLineReader &t); //dom changed
+
+    //Constructor
     FileLineReader();
 
-    // Getters
+    //Copy constructor
+    FileLineReader(FileLineReader &lineReader);
+
+    //Getters & Setters
     vector<string*> getRawCommands();
 
-    //destructor
+    //Destructor
     ~FileLineReader();
 
+    //Methods
+    void readLineFromFile();
 };
 
 
@@ -92,3 +149,4 @@ ostream &operator << (ostream &output, FileLineReader &obj);
 
 
 #endif //RISK_PROJECT_COMMANDPROCESSING_H
+
